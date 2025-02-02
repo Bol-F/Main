95c50d57424dd91c3fa753729b43bd16d90554db
Bol-F <bolievfirdavs0@gmail.com>
2025-02-02 20:44:17 +0500

    i did my norms

---
+++ norms/norm_9.py
@@ -0,0 +1,34 @@
+import csv
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="newfile_9.log",
+    format='%(asctime)s -%(levelname)s- %(message)s',
+    filemode='a'
+)
+logger = logging.getLogger()
+
+
+try:
+    with open("norm_9.txt", newline="", encoding="utf-8") as file:
+        # file.read(0)
+        reader = file.read()
+        print(reader)
+except FileNotFoundError:
+    logger.warning("File not found")
+
+
+# with open("norm_9.txt", mode="w", newline="") as file:
+#     file.write("this is a new line ")
+#     file.write("this is another line  ")
+#     file.write("this is another line ")
+
+
+try:
+    with open("norm_9_2.txt", newline="", encoding="utf-8") as file:
+        read = file.read()
+        print(read)
+except FileNotFoundError:
+    logger.warning("There is no such file")
+    raise FileNotFoundError("This file does not exist")
