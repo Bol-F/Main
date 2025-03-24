f51e29117532ac74ea16bd26cc1055ef16ed3f3d
Bol-F <bolievfirdavs0@gmail.com>
2025-03-24 20:21:33 +0500

    new

--- manage.py
+++ manage.py
@@ -1,22 +1,11 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+# Add the project root to Python's path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.base')
 
+from django.core.management import execute_from_command_line
 
-if __name__ == '__main__':
-    main()
+execute_from_command_line(sys.argv)
--- qwerty/func.py
+++ qwerty/func.py
@@ -1,8 +1,13 @@
-def find_the_multiplication(a, b, c):
-    return a * b * c
+# def find_the_multiplication(a, b, c):
+#     return a * b * c
+#
+# def find_the_factorial(a):
+#     if a == 0 or a == 1:
+#         return 1
+#     else:
+#         return a * find_the_factorial(a - 1)
+import numpy
+import requests
 
-def find_the_factorial(a):
-    if a == 0 or a == 1:
-        return 1
-    else:
-        return a * find_the_factorial(a - 1)
+print(numpy.__version__)
+print(requests.__version__)
