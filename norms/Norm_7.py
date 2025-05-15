992daa7d30f412dc73cf1ff9b6665b114489b7d4
Bol-F <bolievfirdavs0@gmail.com>
2025-05-16 02:38:48 +0500

    norm7

---
+++ norms/Norm_7.py
@@ -0,0 +1,18 @@
+with open('students.txt', 'r') as f:
+    content = f.read()
+
+updated_lines = []
+lines = content.split('\n')
+
+for i in lines:
+    if '-' in i:
+        name, score_str = i.split('-')
+        score = int(score_str)
+        if score < 80:
+            score += 5
+        updated_lines.append(f'{name} - {score}')
+    else:
+        updated_lines.append(i)
+
+with open('students.txt', 'w') as f:
+    f.write('\n'.join(updated_lines))
